5be464bcffc9c855ea3e907147d52764d4456a20
Ramya <ramyaannamalai2002@gmail.com>
2023-06-20 11:02:15 +0530

    First commit
    First Date: 20-06-2023

---
+++ Attendance.sql
@@ -0,0 +1,16 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+CREATE TABLE details(
+    'Name'varchar(40)NOT NULL, 
+    'Degree' varchar(50)NOT NULL,
+    'Email Id' varchar(20)NOT NULL,
+    'Department'varchar(25)NOT NULL,
+    'DOB'varchar(10)NOT NULL,
+    'Gender'varchar(10)NOT NULL,
+    'Mobile no'int(11)NOT NULL,
+    'Experience'int(5)NOT NULL,
+    PRIMARY KEY('Email Id')
+);
+INSERT INTO details('Name','Degree','Email Id','Department','DOB','Gender','Mobile no','Experience')VALUES('Shabhana A','ME-CSE','shabhab439@gmail.com','CSE','01-04-1994','Female',8765483905,5),
+INSERT INTO details('Name','Degree','Email Id','Department','DOB','Gender','Mobile no','Experience')VALUES('Harini G','MBA','harinigovindharaju8@gamil.com','IT','14-03-1990','Female',6485437843,8);
+INSERT INTO details('Details','Excel 12.0';Database='C:\Users\Hari\Documents\project\1\project\Details.csv',SELECT*FROM['sheet1.$']SELECT Name FROM Degree where Experience=1)
